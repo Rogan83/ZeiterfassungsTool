49f9d4aac9a7343dae3503dc4afda8b32df0e9da
Daniel Rothweiler <D.Rothweiler@yahoo.de>
2023-03-08 15:22:26 +0100

    User: Anzeige implementiert, wo in den ausgewählten Monat die Anzahl der Stunden, die Anzahl der fehlenden Stunden bis das Monatsziel erreicht ist und die Anzahl der Überstunden angezeigt wird.

--- ZeiterfassungsTool/MVVM/ViewModels/User/UserWorkingHoursModel.cs
+++ ZeiterfassungsTool/MVVM/ViewModels/User/UserWorkingHoursModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PropertyChanged;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +9,44 @@ using ZeiterfassungsTool.StaticClasses;
 
 namespace ZeiterfassungsTool.MVVM.ViewModels.User
 {
+    [AddINotifyPropertyChangedInterface]
     public class UserWorkingHoursModel
     {
+        private string month;
+
+        public string Month
+        {
+            get => month; set
+            {
+                bool parseSuccessful = Int32.TryParse(value, out int result);
+                if (parseSuccessful)
+                {
+                    if (result > 12)
+                        result = 12;
+                    else if (result < 0)
+                        result = 0;
+                    value = result.ToString();
+
+                }
+
+                month = value;
+            }
+        }
+        public string Year { get; set; }
+
+        public string Percent { get; set; }
+
         public double HoursToWorkThisMonth { get; set; }
 
         public double WorkingHours { get; set; }
+        public double ShapePointer { get; set; }
 
-        public Color Color { get; set; }
+        public double HoursLeft { get; set; }
+
+        public double Overtime { get; set; }
+
+        public Color ColorRadial { get; set; }
+        public Color ColorPointer { get; set; }
 
         public UserWorkingHoursModel()
         {
@@ -24,7 +56,11 @@ namespace ZeiterfassungsTool.MVVM.ViewModels.User
 
             HoursToWorkThisMonth = GetWorkingDaysPerMonth(workingHoursPerDay);
 
-            WorkingHours = WorkingHoursThisMonth();
+            Month = DateTime.Now.Month.ToString();
+            Year = DateTime.Now.Year.ToString();
+
+            ColorRadial = Colors.Red;
+            ColorPointer = Colors.DarkRed;
         }
 
         private double GetWorkingDaysPerMonth(double workingHoursPerDay)
@@ -54,10 +90,35 @@ namespace ZeiterfassungsTool.MVVM.ViewModels.User
             return workingHoursPerMonth;
         }
 
+
         private double WorkingHoursThisMonth()
         {
+            
             double hours = 0;
-            var currentMonth = DateTime.Now.Month;  
+            int currentMonth = 0;
+            if (Month != null)
+            {
+                if(Month.Length == 0)
+                {
+                    App.Current.MainPage.DisplayAlert("Ungültige Eingabe", "Sie müssen min. 1 Zeichen im Feld Monat eingeben", "OK");
+                    return 0;
+                }
+                currentMonth = Int32.Parse(Month);      //DateTime.Now.Month;
+            }
+            //
+            int currentYear = 0;
+            if (Year != null)
+            {
+                if (Year.Length < 4)
+                {
+                    App.Current.MainPage.DisplayAlert("Ungültige Eingabe", "Sie müssen min. 4 Zeichen im Feld Jahr eingeben", "OK");
+                    return 0;
+                }
+                currentYear = Int32.Parse(Year);        //DateTime.Now.Year;
+            }
+
+            
+
 
             var employee = SaveLoginStatus.WhoIsLoggedIn[0];
 
@@ -66,14 +127,14 @@ namespace ZeiterfassungsTool.MVVM.ViewModels.User
                 if (timetracking.Subject == "Urlaub" || timetracking.Subject == "Krank")
                     continue;
 
-                if (timetracking.StartTime.Month == currentMonth && timetracking.EndTime.Month == currentMonth)
+                if (timetracking.StartTime.Month == currentMonth && timetracking.EndTime.Month == currentMonth && timetracking.StartTime.Year == currentYear && timetracking.EndTime.Year == currentYear)
                 {
                     TimeSpan timeSpan = timetracking.EndTime - timetracking.StartTime;
 
                     hours += timeSpan.TotalHours;
-                    
+
                 }
-                else if (timetracking.StartTime.Month == currentMonth)
+                else if (timetracking.StartTime.Month == currentMonth && timetracking.StartTime.Year == currentYear)
                 {
                     TimeOnly timeInTimetrackingStart = TimeOnly.FromDateTime(timetracking.StartTime);
                     TimeOnly endOfDay = new TimeOnly(0, 0);
@@ -82,7 +143,7 @@ namespace ZeiterfassungsTool.MVVM.ViewModels.User
 
                     hours += difference.TotalHours;
                 }
-                else if (timetracking.EndTime.Month == currentMonth)
+                else if (timetracking.EndTime.Month == currentMonth && timetracking.EndTime.Year == currentYear)
                 {
                     TimeOnly timeInTimetrackingEnd = TimeOnly.FromDateTime(timetracking.EndTime);
                     TimeOnly startOfDay = new TimeOnly(0, 0);
@@ -95,21 +156,41 @@ namespace ZeiterfassungsTool.MVVM.ViewModels.User
 
             if (hours < HoursToWorkThisMonth)
             {
-                Color = Colors.Red;
+                ColorRadial = Colors.Red;
+                ColorPointer = Colors.Red;
+
+                Overtime = 0;
+                ShapePointer = hours;
+                HoursLeft = (int)(HoursToWorkThisMonth - hours);
             }
             else
             {
-                hours -= HoursToWorkThisMonth;
-                Color = Colors.Green;
+                ColorRadial = Colors.Green;
+                ColorPointer = Colors.DarkGreen;
+
+                Overtime = (int)(hours - HoursToWorkThisMonth);
+                ShapePointer = Overtime;
+                HoursLeft = 0;
             }
 
-            return hours;
+            
+
+            var percent = (int)(hours / HoursToWorkThisMonth * 100);
+            Percent = $"{percent}%";
+
+            return (int)hours;
         }
 
         public ICommand BackButton =>
          new Command(() =>
          {
              Shell.Current.GoToAsync("UserWorkingHours/UserPage");
+         });
+
+        public ICommand ShowWorkingHours =>
+         new Command(() =>
+         {
+             WorkingHours = WorkingHoursThisMonth();
          });
     }
 }
